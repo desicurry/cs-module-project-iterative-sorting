1dc5ddd2926f4f1959d7d6415794ea81eb5f12c7
Desi Curry <Blackice@Desis-MacBook-Air.local>
2020-09-29 21:00:45 -0400

    MVP + Stretch

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    if len(arr) == 0:
+        return -1
 
+    left = 0
+    right = len(arr)
+    while left <= right:
+        mid = int(left + (right - 1) / 2)
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
 
     return -1  # not found
